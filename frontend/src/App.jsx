e74e8e51888a1fd772b33ee8d4eb47efcf9c527c
Gaurav Bansal <95044674+gbgaurav007@users.noreply.github.com>
2025-05-14 12:49:52 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,11 @@ import EventListPage from './pages/EventListPage';
 import HomePage from './pages/HomePage';
 import Account from './pages/Account';
 import EventDetailPage from './pages/EventDetailPage';
+import Terms from './pages/Terms';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import RefundPolicy from './pages/RefundPolicy';
+import Contact from './pages/Contact';
+import Payment from './pages/Payment';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
 
@@ -25,6 +30,11 @@ function App() {
         <Route path="/home" element={<HomePage userData={userData}/>} />
         <Route path="/events" element={<EventListPage />} />
         <Route path="/details" element={<EventDetailPage userData={userData}/>} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/refund" element={<RefundPolicy />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/payment" element={<Payment />} />
       </Routes>
     </div>
 
