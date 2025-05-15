bf7d61c46c429bcf741d230f4ebca6c6cd96d71c
Gaurav Bansal <95044674+gbgaurav007@users.noreply.github.com>
2025-05-16 02:03:54 +0530

    Update BookedEventCard.jsx

--- frontend/src/components/BookedEventCard.jsx
+++ frontend/src/components/BookedEventCard.jsx
@@ -1,10 +1,18 @@
 import { IoIosArrowForward } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 const BookedEventCard = ({ event }) => {
+
+  const navigate = useNavigate();
+
+  const handleViewDetails = (event) => {
+    navigate('/details', { state: { event } });
+};
+
   return (
     <div className="bg-white shadow rounded-lg p-4">
       <div className="flex space-x-6">
-        <img src={event.image} alt={event.name} className="w-40 h-40 md:w-48 md:h-48 object-cover rounded" />
+        <img src={event.images[0]} alt={event.name} className="w-40 h-40 md:w-48 md:h-48 object-cover rounded" />
         <div className="w-full">
           <h2 className="text-xl font-bold">{event.name}</h2>
           <p className="text-gray-600 text-md">{event.category}</p>
@@ -24,7 +32,7 @@ const BookedEventCard = ({ event }) => {
       <hr className="h-px my-3 bg-gray-300 border-0 md:hidden block"></hr>
       <div className="flex justify-between md:hidden block">
         <p className="text-blue-600">{event.tickets} ticket(s) booked</p>
-        <div className="flex cursor-pointer hover:text-gray-700 hover:scale-[1.05]">
+        <div className="flex cursor-pointer hover:text-gray-700 hover:scale-[1.05]" onClick={() => handleViewDetails(event)}>
           <p>View Details</p><IoIosArrowForward className="text-md mt-1 ml-1" />
         </div>
       </div>
